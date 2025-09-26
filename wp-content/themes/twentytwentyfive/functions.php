9b8605fde88b4c46ee0be821bcda521d77b1f4fa
nbaonguyen111 <baonguyenmiennam@gmail.com>
2025-09-26 11:14:24 +0700

    Final Custom

--- wp-content/themes/twentytwentyfive/functions.php
+++ wp-content/themes/twentytwentyfive/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Twenty Twenty-Five functions and definitions.
  *
@@ -9,38 +10,38 @@
  * @since Twenty Twenty-Five 1.0
  */
 
-
- function custom_footer_widgets_init() {
-    register_sidebar(array(
-        'name'          => 'Footer Column 1',
-        'id'            => 'footer-1',
-        'before_widget' => '<div class="footer-widget">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h4>',
-        'after_title'   => '</h4>',
-    ));
-    register_sidebar(array(
-        'name'          => 'Footer Column 2',
-        'id'            => 'footer-2',
-        'before_widget' => '<div class="footer-widget">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h4>',
-        'after_title'   => '</h4>',
-    ));
-    register_sidebar(array(
-        'name'          => 'Footer Column 3',
-        'id'            => 'footer-3',
-        'before_widget' => '<div class="footer-widget">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h4>',
-        'after_title'   => '</h4>',
-    ));
+//Footer #1
+function custom_footer_widgets_init()
+{
+	register_sidebar(array(
+		'name'          => 'Footer Column 1',
+		'id'            => 'footer-1',
+		'before_widget' => '<div class="footer-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h5>',
+		'after_title'   => '</h5>',
+	));
+	register_sidebar(array(
+		'name'          => 'Footer Column 2',
+		'id'            => 'footer-2',
+		'before_widget' => '<div class="footer-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h5>',
+		'after_title'   => '</h5>',
+	));
+	register_sidebar(array(
+		'name'          => 'Footer Column 3',
+		'id'            => 'footer-3',
+		'before_widget' => '<div class="footer-widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h5>',
+		'after_title'   => '</h5>',
+	));
 }
 add_action('widgets_init', 'custom_footer_widgets_init');
 
-
 // Adds theme support for post formats.
-if ( ! function_exists( 'twentytwentyfive_post_format_setup' ) ) :
+if (! function_exists('twentytwentyfive_post_format_setup')) :
 	/**
 	 * Adds theme support for post formats.
 	 *
@@ -48,14 +49,15 @@ if ( ! function_exists( 'twentytwentyfive_post_format_setup' ) ) :
 	 *
 	 * @return void
 	 */
-	function twentytwentyfive_post_format_setup() {
-		add_theme_support( 'post-formats', array( 'aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video' ) );
+	function twentytwentyfive_post_format_setup()
+	{
+		add_theme_support('post-formats', array('aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video'));
 	}
 endif;
-add_action( 'after_setup_theme', 'twentytwentyfive_post_format_setup' );
+add_action('after_setup_theme', 'twentytwentyfive_post_format_setup');
 
 // Enqueues editor-style.css in the editors.
-if ( ! function_exists( 'twentytwentyfive_editor_style' ) ) :
+if (! function_exists('twentytwentyfive_editor_style')) :
 	/**
 	 * Enqueues editor-style.css in the editors.
 	 *
@@ -63,14 +65,15 @@ if ( ! function_exists( 'twentytwentyfive_editor_style' ) ) :
 	 *
 	 * @return void
 	 */
-	function twentytwentyfive_editor_style() {
-		add_editor_style( 'assets/css/editor-style.css' );
+	function twentytwentyfive_editor_style()
+	{
+		add_editor_style('assets/css/editor-style.css');
 	}
 endif;
-add_action( 'after_setup_theme', 'twentytwentyfive_editor_style' );
+add_action('after_setup_theme', 'twentytwentyfive_editor_style');
 
 // Enqueues style.css on the front.
-if ( ! function_exists( 'twentytwentyfive_enqueue_styles' ) ) :
+if (! function_exists('twentytwentyfive_enqueue_styles')) :
 	/**
 	 * Enqueues style.css on the front.
 	 *
@@ -78,19 +81,20 @@ if ( ! function_exists( 'twentytwentyfive_enqueue_styles' ) ) :
 	 *
 	 * @return void
 	 */
-	function twentytwentyfive_enqueue_styles() {
+	function twentytwentyfive_enqueue_styles()
+	{
 		wp_enqueue_style(
 			'twentytwentyfive-style',
-			get_parent_theme_file_uri( 'style.css' ),
+			get_parent_theme_file_uri('style.css'),
 			array(),
-			wp_get_theme()->get( 'Version' )
+			wp_get_theme()->get('Version')
 		);
 	}
 endif;
-add_action( 'wp_enqueue_scripts', 'twentytwentyfive_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'twentytwentyfive_enqueue_styles');
 
 // Registers custom block styles.
-if ( ! function_exists( 'twentytwentyfive_block_styles' ) ) :
+if (! function_exists('twentytwentyfive_block_styles')) :
 	/**
 	 * Registers custom block styles.
 	 *
@@ -98,12 +102,13 @@ if ( ! function_exists( 'twentytwentyfive_block_styles' ) ) :
 	 *
 	 * @return void
 	 */
-	function twentytwentyfive_block_styles() {
+	function twentytwentyfive_block_styles()
+	{
 		register_block_style(
 			'core/list',
 			array(
 				'name'         => 'checkmark-list',
-				'label'        => __( 'Checkmark', 'twentytwentyfive' ),
+				'label'        => __('Checkmark', 'twentytwentyfive'),
 				'inline_style' => '
 				ul.is-style-checkmark-list {
 					list-style-type: "\2713";
@@ -116,10 +121,10 @@ if ( ! function_exists( 'twentytwentyfive_block_styles' ) ) :
 		);
 	}
 endif;
-add_action( 'init', 'twentytwentyfive_block_styles' );
+add_action('init', 'twentytwentyfive_block_styles');
 
 // Registers pattern categories.
-if ( ! function_exists( 'twentytwentyfive_pattern_categories' ) ) :
+if (! function_exists('twentytwentyfive_pattern_categories')) :
 	/**
 	 * Registers pattern categories.
 	 *
@@ -127,29 +132,30 @@ if ( ! function_exists( 'twentytwentyfive_pattern_categories' ) ) :
 	 *
 	 * @return void
 	 */
-	function twentytwentyfive_pattern_categories() {
+	function twentytwentyfive_pattern_categories()
+	{
 
 		register_block_pattern_category(
 			'twentytwentyfive_page',
 			array(
-				'label'       => __( 'Pages', 'twentytwentyfive' ),
-				'description' => __( 'A collection of full page layouts.', 'twentytwentyfive' ),
+				'label'       => __('Pages', 'twentytwentyfive'),
+				'description' => __('A collection of full page layouts.', 'twentytwentyfive'),
 			)
 		);
 
 		register_block_pattern_category(
 			'twentytwentyfive_post-format',
 			array(
-				'label'       => __( 'Post formats', 'twentytwentyfive' ),
-				'description' => __( 'A collection of post format patterns.', 'twentytwentyfive' ),
+				'label'       => __('Post formats', 'twentytwentyfive'),
+				'description' => __('A collection of post format patterns.', 'twentytwentyfive'),
 			)
 		);
 	}
 endif;
-add_action( 'init', 'twentytwentyfive_pattern_categories' );
+add_action('init', 'twentytwentyfive_pattern_categories');
 
 // Registers block binding sources.
-if ( ! function_exists( 'twentytwentyfive_register_block_bindings' ) ) :
+if (! function_exists('twentytwentyfive_register_block_bindings')) :
 	/**
 	 * Registers the post format block binding source.
 	 *
@@ -157,20 +163,21 @@ if ( ! function_exists( 'twentytwentyfive_register_block_bindings' ) ) :
 	 *
 	 * @return void
 	 */
-	function twentytwentyfive_register_block_bindings() {
+	function twentytwentyfive_register_block_bindings()
+	{
 		register_block_bindings_source(
 			'twentytwentyfive/format',
 			array(
-				'label'              => _x( 'Post format name', 'Label for the block binding placeholder in the editor', 'twentytwentyfive' ),
+				'label'              => _x('Post format name', 'Label for the block binding placeholder in the editor', 'twentytwentyfive'),
 				'get_value_callback' => 'twentytwentyfive_format_binding',
 			)
 		);
 	}
 endif;
-add_action( 'init', 'twentytwentyfive_register_block_bindings' );
+add_action('init', 'twentytwentyfive_register_block_bindings');
 
 // Registers block binding callback function for the post format name.
-if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
+if (! function_exists('twentytwentyfive_format_binding')) :
 	/**
 	 * Callback function for the post format name block binding source.
 	 *
@@ -178,13 +185,14 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 	 *
 	 * @return string|void Post format name, or nothing if the format is 'standard'.
 	 */
-	function twentytwentyfive_format_binding() {
+	function twentytwentyfive_format_binding()
+	{
 		$post_format_slug = get_post_format();
 
-		if ( $post_format_slug && 'standard' !== $post_format_slug ) {
-			return get_post_format_string( $post_format_slug );
+		if ($post_format_slug && 'standard' !== $post_format_slug) {
+			return get_post_format_string($post_format_slug);
 		}
 	}
 
-	
+
 endif;
--- wp-content/themes/twentytwentyfive/patterns/footer.php
+++ wp-content/themes/twentytwentyfive/patterns/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Footer
  * Slug: twentytwentyfive/footer
@@ -12,67 +13,64 @@
  */
 ?>
 
-<footer id="colophon" class="site-footer" role="contentinfo" style="background-color:#007b5e; color:#ffffff; padding:60px 0; text-align:center;">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-    <!-- Site Logo -->
-    <div class="footer-logo" style="margin-bottom:30px;">
-        <?php if ( function_exists( 'the_custom_logo' ) ) {
-            the_custom_logo();
-        } ?>
+
+<!-- Footer -->
+<section id="footer">
+    <div class="container">
+        <div class="row text-center text-xs-center text-sm-left text-md-left">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+               
+                <div class="footer-column ">
+                    <?php if (is_active_sidebar('<i class="fa fa-angle-double-right"></i> footer-1')) : ?>
+                        <?php dynamic_sidebar('footer-1'); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                
+                <div class="footer-column">
+                    <?php if (is_active_sidebar('footer-2')) : ?>
+                        <?php dynamic_sidebar('footer-2'); ?>
+                    <?php endif; ?>
+                </div>
+
+
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <div class="footer-column">
+                    <?php if (is_active_sidebar('footer-3')) : ?>
+                        <?php dynamic_sidebar('footer-3'); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <!-- Social Icons -->
-    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","verticalAlignment":"center"}} -->
-    <div class="footer-social" style="margin-bottom:30px;">
-        <a href="#" class="social-icon"><i class="fa fa-facebook"></i></a>
-        <a href="#" class="social-icon"><i class="fa fa-twitter"></i></a>
-        <a href="#" class="social-icon"><i class="fa fa-instagram"></i></a>
-        <a href="#" class="social-icon"><i class="fa fa-google"></i></a>
-        <a href="#" class="social-icon"><i class="fa fa-envelope"></i></a>
-        <a href="#" class="social-icon"><i class="fa fa-linkedin"></i></a>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+            <ul class="list-unstyled list-inline social text-center">
+                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
+                <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+            </ul>
+        </div>
+        <hr>
     </div>
-    <!-- /wp:group -->
-
-    <!-- Footer Menu -->
-    <div class="wp-block-group" style="margin-bottom:30px;">
-        <?php
-        $menu_name = 'Footer Menu';
-        $menu = wp_get_nav_menu_object($menu_name);
-        if ($menu):
-        ?>
-            <!-- wp:navigation {"ref":<?php echo $menu->term_id; ?>,"overlayMenu":"never","layout":{"type":"flex","orientation":"horizontal","justifyContent":"center"}} /-->
-        <?php endif; ?>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+            <p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+            <p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+        </div>
+        <hr>
     </div>
-      <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","verticalAlignment":"center"}} -->
-	<div class="footer-widgets">
-    <div class="footer-column">
-        <?php if (is_active_sidebar('footer-1')) : ?>
-            <?php dynamic_sidebar('footer-1'); ?>
-        <?php endif; ?>
     </div>
-    <div class="footer-column">
-        <?php if (is_active_sidebar('footer-2')) : ?>
-            <?php dynamic_sidebar('footer-2'); ?>
-        <?php endif; ?>
-    </div>
-    <div class="footer-column">
-        <?php if (is_active_sidebar('footer-3')) : ?>
-            <?php dynamic_sidebar('footer-3'); ?>
-        <?php endif; ?>
-    </div>
-</div>
-<!-- /wp:group -->
-   
-
-    <!-- Footer Text -->
-    <div class="footer-text">
-        <p>© <?php echo date('Y'); ?> Your Website. All rights reserved.</p>
-        <p>Privacy Policy | Terms of Service</p>
-    </div>
-
-
-
-</footer>
-
+</section>
+<!-- ./Footer -->
 <!-- Font Awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
