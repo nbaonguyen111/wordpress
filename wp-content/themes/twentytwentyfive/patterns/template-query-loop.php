447038e883e2b7f9eac2b939db052339ce7d5e57
nbaonguyen111 <baonguyenmiennam@gmail.com>
2025-09-26 17:25:09 +0700

    Custom_Post

--- wp-content/themes/twentytwentyfive/patterns/template-query-loop.php
+++ wp-content/themes/twentytwentyfive/patterns/template-query-loop.php
@@ -13,17 +13,67 @@
 
 ?>
 <!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"align":"full","layout":{"type":"default"}} -->
-<div class="wp-block-query alignfull">
-	<!-- wp:post-template {"align":"full","layout":{"type":"default"}} -->
-		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2"} /-->
-			<!-- wp:post-title {"isLink":true,"fontSize":"x-large"} /-->
-			<!-- wp:post-content {"align":"full","fontSize":"medium","layout":{"type":"constrained"}} /-->
-			<!-- wp:post-date {"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"fontSize":"small"} /-->
-		</div>
-		<!-- /wp:group -->
-	<!-- /wp:post-template -->
+<div class="wp-block-query alignwide ">
+	<!-- wp:post-template -->
+    <div class="wp-block-group md:grid md:grid-cols-2 bg-white border border-gray-100 shadow-md overflow-hidden mb-6 group post-item">
+        <!-- Thumbnail -->
+        <!-- wp:group {"className":"col-span-4 overflow-hidden flex items-center"} -->
+        <div class="wp-block-group col-span-5 overflow-hidden flex items-center">
+            <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/4"} /-->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- Content -->
+        <!-- wp:group {"className":"col-span-7 p-6 flex flex-col justify-between"} -->
+        <div class="wp-block-group col-span-7 p-6 d-flex justify-between">
+            
+            <!-- Date + Title -->
+            <!-- wp:group {"className":"flex flex-col md:flex-row mb-4"} -->
+            <div class="wp-block-group flex flex-col md:flex-row mb-4">
+                
+                <!-- Date box -->
+                <div class="datebox flex items-center ">
+					<div class="day">
+						<!-- wp:post-date {"format":"d","isLink":false,"style":{"typography":{"fontSize":"2rem"}}} /-->
+
+					</div>
+
+                    <div class="month-year flex flex-col text-sm leading-tight ml-2">
+                        <span class="uppercase text-sm text-gray-500 whitespace-nowrap">Tháng <!-- wp:post-date {"format":"n","isLink":false} /--></span>
+                        <span class="text-blue-600 text-sm font-medium"><!-- wp:post-date {"format":"Y","isLink":false} /--></span>
+                    </div>
+                </div>
+
+                <!-- Title + Category -->
+                <div>
+					<div class="title">
+						<!-- wp:post-title {"isLink":true,"className":"text-xl font-bold text-gray-900 mb-2 post-title line-clamp-2 mt-[10px] md:mt-0"} /-->
+
+					</div>
+                    
+                    <div class="text-sm text-gray-600 space-x-4">
+                        <div class="flex cateflex">
+                            <div class="font-medium text-gray-800">Danh Mục: <!-- wp:post-terms {"term":"category"} /--></div>
+                    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /wp:group -->
+
+            <!-- Summary -->
+			<div class="text-gray-700 text-sm line-clamp-2 excerpt">
+				<!-- wp:post-excerpt {"moreText":"Xem thêm"} /-->
+			</div>
+        </div>
+        <!-- /wp:group -->
+		
+    </div>
+
+    <!-- /wp:group -->
+<!-- /wp:post-template -->
+</div>
+
 	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
 		<!-- wp:query-no-results -->
